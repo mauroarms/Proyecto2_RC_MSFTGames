cb6c68362d35b2b5c537f760c7256a400dbfb81e
lucianocabelloo <you@example.com>
2023-12-06 15:02:35 -0300

    Añadida las funciones guardar en localstorage y recuperar datos de localstorage para que los juegos persistan

--- js/administrador.js
+++ js/administrador.js
@@ -2,6 +2,7 @@ import Juego from "./class.js";
 
 const listaDeJuegos = [];
 const formularioDeJuegos = document.querySelector("#formAdministrarJuego");
+const table = document.querySelector(".table tbody");
 
 const crearJuego = (e) => {
   e.preventDefault();
@@ -25,34 +26,58 @@ const crearJuego = (e) => {
   );
 
   listaDeJuegos.push(juegoNuevo);
-  console.log(listaDeJuegos);
+  guardarLocalStorage();
   añadirJuego(juegoNuevo);
 };
 
 const añadirJuego = (juegoNuevo) => {
-    const table = document.querySelector(".table tbody");
-    const fila = document.createElement("tr");
+  const fila = document.createElement("tr");
 
-    fila.innerHTML = `
-        <th scope="row">${table.rows.length + 1}</th>
-        <td>${juegoNuevo.getTitulo()}</td>
-        <td class="text-truncate">${juegoNuevo.getDescripcion()}</td>
-        <td class="text-center"> <img src=${juegoNuevo.getImagen()} height="150" width="150"></td>
-        <td>${juegoNuevo.getGenero()}</td>
-        <td>${juegoNuevo.getCreador()}</td>
-        <td>${juegoNuevo.getLanzamiento()}</td>
-        <td>${juegoNuevo.getEdad()}</td>
-        <td>
-            <div class="d-flex gap-3">
-                <button class="btn btn-warning">Editar</button>
-                <button class="btn btn-danger">Borrar</button>
-            </div>
-        </td>
-    `;
+  fila.innerHTML = `
+    <th scope="row">${table.rows.length + 1}</th>
+    <td>${juegoNuevo.getTitulo()}</td>
+    <td class="text-truncate">${juegoNuevo.getDescripcion()}</td>
+    <td class="text-center"> <img src=${juegoNuevo.getImagen()} height="150" width="150"></td>
+    <td>${juegoNuevo.getGenero()}</td>
+    <td>${juegoNuevo.getCreador()}</td>
+    <td>${juegoNuevo.getLanzamiento()}</td>
+    <td>${juegoNuevo.getEdad()}</td>
+    <td>
+        <div class="d-flex gap-3">
+            <button class="btn btn-warning">Editar</button>
+            <button class="btn btn-danger">Borrar</button>
+        </div>
+    </td>
+  `;
 
-    table.appendChild(fila);
-    formularioDeJuegos.reset();
+  table.appendChild(fila);
 };
 
+const guardarLocalStorage = () => {
+  localStorage.setItem("listado", JSON.stringify(listaDeJuegos));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const datosGuardados = localStorage.getItem("listado");
+
+  if (datosGuardados) {
+    const juegosGuardados = JSON.parse(datosGuardados);
+    juegosGuardados.forEach((juego) => {
+      const juegoObj = new Juego(
+        undefined,
+        juego.titulo,
+        juego.descripcion,
+        juego.imagen,
+        juego.genero,
+        juego.creador,
+        juego.lanzamiento,
+        juego.edad
+      );
+
+      listaDeJuegos.push(juegoObj);
+      añadirJuego(juegoObj);
+    });
+  }
+});
 
 formularioDeJuegos.addEventListener("submit", crearJuego);

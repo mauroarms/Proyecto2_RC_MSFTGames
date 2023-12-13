2a74c89b150a7646473d99c370b55fee6d892f08
mauroarms <mauro.armas14@gmail.com>
2023-12-13 16:16:58 -0300

    Se agregó una navegación de juegos en el detalle del producto

--- js/jsProducto.js
+++ js/jsProducto.js
@@ -119,3 +119,56 @@ const graficarJuego = (Juego) =>{
 graficarJuego(juego);
 
 
+const getSize = () => {
+    if (screen.width <= 350) {
+        return 1;
+    } else if (screen.width <= 750 && screen.width > 500) {
+        return 2;
+    } else if (screen.width <= 990 && screen.width > 750) {
+        return 3;
+    } else {
+        return 4;
+    }
+};
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: false,
+    grabCursor: true,
+    slidesPerView: getSize(),
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+const renderJuegos = () => {
+    const contenedor = document.querySelector(".swiper-wrapper");
+    console.log(contenedor)
+    if (!juegos.length) {
+        contenedor.innerHTML = `<h2 class="display-3 text-center mx-auto">No hay juegos guardados</h2>`;
+    }
+    juegos?.map((juego) => {
+        if(juego.codigo !== codigoJuego){
+            contenedor.innerHTML += 
+            `<div class="swiper-slide flex-column">
+                <a href="${window.location.origin}/pages/detalleProducto.html?codigo=${juego.codigo}" class="game" class="juego">
+                    <img src="${juego.imagen}" alt="${juego.descripcion}" />
+                    <div class="card-body">
+                        <h3 class="card-title pt-3 text-center">
+                            ${juego.titulo}    
+                        </h3>
+                        <h5 class="card-title py-4 text-center">
+                            ${juego.creador}    
+                        </h5>
+                    </div>
+                </a>
+            </div>`
+        }
+    });
+};
+
+renderJuegos();
+console.log(codigoJuego)
